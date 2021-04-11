8064f933c35dc2e73c7f328651a9bc87ab1ac7a0
simon-ugor <simiugor@gmail.com>
2021-04-11 20:16:44 +0200

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import "../styles.css"
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
-  
-  export default MyApp
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
+}
+
+export default App
--- pages/word/[word].js
+++ pages/word/[word].js
@@ -33,7 +33,7 @@ const Word = () => {
         let url = window.location.href
 
         WordsData.map(d => {
-            if (url === "https://lingomeaning.com/" + d.link) {
+            if (url === "http://localhost:3000/" + d.link) {
                 if (d.meaning1 && d.meaning2 && d.meaning3 && d.meaning4){
                     setAmount(4)
                     setWord1(d.word)
